5f2597ed234645a6694b86ec6955ab72d079902f
Leart-Loen <learturely@gmail.com>
2023-11-13 20:48:33 +0800

    要提交的变更： 	新文件：   src/cli.rs 	新文件：   src/gui.rs 	新文件：   src/utils/sql.rs 	新文件：   ui/accountlist.slint 	新文件：   ui/activitylist.slint 	新文件：   ui/assets/icon/account.svg 	新文件：   ui/assets/icon/activity.svg 	新文件：   ui/assets/icon/course.svg 	新文件：   "\350\266\205\346\230\237\347\255\276\345\210\260api\350\256\260\345\275\225.md"

---
+++ src/cli.rs
@@ -0,0 +1,392 @@
+use std::{
+    collections::{hash_map::OccupiedError, HashMap},
+    fs::DirEntry,
+    path::PathBuf,
+    time::SystemTime,
+};
+pub fn print_now() {
+    let str = chrono::DateTime::<chrono::Local>::from(SystemTime::now())
+        .format("%+")
+        .to_string();
+    println!("{str}");
+}
+use crate::{
+    sign_session::{
+        activity::{
+            activity::{Address, SignActivity, SignState},
+            sign::{Photo, SignType},
+        },
+        sign_session::SignSession,
+    },
+    utils::sql::DataBase,
+};
+
+// 添加账号。
+pub async fn add_account(db: &DataBase, uname: String, pwd: Option<String>) {
+    let pwd = if let Some(pwd) = pwd {
+        pwd
+    } else {
+        inquire::Password::new("密码：")
+            .without_confirmation()
+            .prompt()
+            .unwrap()
+    };
+    let session = SignSession::login(&uname, &pwd).await.unwrap();
+    let name = session.get_stu_name();
+    db.add_account_or(&uname, &pwd, name, DataBase::update_account);
+    let courses = session.get_courses().await.unwrap();
+    for c in courses {
+        db.add_course_or(&c, |_, _| {});
+    }
+}
+
+pub async fn get_sessions(db: &DataBase) -> HashMap<String, SignSession> {
+    let accounts = db.get_accounts();
+    let config_dir = crate::utils::CONFIG_DIR.clone();
+    let mut s = HashMap::new();
+    for a in accounts {
+        let cookies_dir = config_dir.join(a.0.to_string() + ".json");
+        let session = SignSession::load(cookies_dir).await.unwrap();
+        s.insert(a.0, session);
+    }
+    s
+}
+
+pub async fn get_signs<'a>(
+    sessions: &'a HashMap<String, SignSession>,
+) -> (
+    HashMap<SignActivity, Vec<&'a SignSession>>,
+    HashMap<SignActivity, Vec<&'a SignSession>>,
+) {
+    let mut asigns = HashMap::new();
+    let mut osigns = HashMap::new();
+    for (_, session) in sessions {
+        let (available_sign_activities, other_sign_activities, _) =
+            session.traverse_activities().await.unwrap();
+        for sa in available_sign_activities {
+            let vec = vec![session];
+            if let Err(OccupiedError {
+                mut entry,
+                value: _,
+            }) = asigns.try_insert(sa, vec)
+            {
+                entry.get_mut().push(session);
+            }
+        }
+        for sa in other_sign_activities {
+            let vec = vec![session];
+            if let Err(OccupiedError {
+                mut entry,
+                value: _,
+            }) = osigns.try_insert(sa, vec)
+            {
+                entry.get_mut().push(session);
+            }
+        }
+    }
+    (asigns, osigns)
+}
+async fn photo_sign_(
+    s: &SignActivity,
+    session: &SignSession,
+    pic: &Option<PathBuf>,
+) -> Result<SignState, reqwest::Error> {
+    if let Some(pic) = pic {
+        let photo = Photo::from_file(session, &pic).await;
+        s.photo_sign(&photo, session).await
+    } else {
+        let photo = Photo::default(session).await;
+        s.photo_sign(&photo, session).await
+    }
+}
+async fn general_sign_(
+    s: &SignActivity,
+    session: &SignSession,
+) -> Result<SignState, reqwest::Error> {
+    s.general_sign(session).await
+}
+async fn qrcode_sign_(
+    s: &SignActivity,
+    session: &SignSession,
+    pic: &Option<PathBuf>,
+    db: &DataBase,
+    enc: &Option<String>,
+    location: Option<i64>,
+    pos: &Option<String>,
+) -> Result<SignState, reqwest::Error> {
+    let enc = {
+        if let Some(enc) = enc {
+            enc.to_owned()
+        } else {
+            if let Some(pic) = pic {
+                let results = rxing::helpers::detect_multiple_in_file(pic.to_str().unwrap())
+                    .expect("decodes");
+                let r = &results[0];
+                let r = r.getText();
+                let beg = r.find("&enc=").unwrap();
+                let s = &r[beg + 5..beg + 37];
+                // println!("enc: {s}");
+                s.to_owned()
+            } else {
+                panic!("二维码签到需要提供 `enc` 参数或签到二维码！")
+            }
+        }
+    };
+    if let Some(addr) = location {
+        let poss = db.get_poss();
+        let pos = &poss[&addr].1;
+        s.qrcode_sign(&enc, &pos, session).await
+    } else if let Some(ref pos) = pos {
+        let pos = Address::parse_str(pos);
+        s.qrcode_sign(&enc, &pos, session).await
+    } else {
+        let mut fail_msg = String::from("未查询到有效的位置信息，二维码签到失败！");
+        let poss = db.get_course_poss(s.course.get_id());
+        for (_, ref pos) in poss {
+            match s.qrcode_sign(&enc, &pos, session).await? {
+                r @ SignState::Success => return Ok(r),
+                SignState::Fail(msg) => {
+                    fail_msg.push_str("\n\t");
+                    fail_msg.push_str(msg.as_str());
+                    continue;
+                }
+            };
+        }
+        let poss = db.get_course_poss(-1);
+        for (_, ref pos) in poss {
+            match s.qrcode_sign(&enc, &pos, session).await? {
+                r @ SignState::Success => return Ok(r),
+                SignState::Fail(msg) => {
+                    fail_msg.push_str("\n\t");
+                    fail_msg.push_str(msg.as_str());
+                    continue;
+                }
+            };
+        }
+        Ok(SignState::Fail(fail_msg))
+    }
+}
+async fn location_sign_(
+    s: &SignActivity,
+    session: &SignSession,
+    db: &DataBase,
+    location: Option<i64>,
+    pos: &Option<String>,
+) -> Result<SignState, reqwest::Error> {
+    if let Some(addr) = location {
+        let poss = db.get_poss();
+        let pos = &poss[&addr].1;
+        s.location_sign(&pos, session).await
+    } else if let Some(ref pos) = pos {
+        let pos = Address::parse_str(pos);
+        s.location_sign(&pos, session).await
+    } else {
+        let mut fail_msg = String::new();
+        let poss = db.get_course_poss(s.course.get_id());
+        for (_, ref pos) in poss {
+            match s.location_sign(&pos, session).await? {
+                r @ SignState::Success => return Ok(r),
+                SignState::Fail(msg) => {
+                    fail_msg.push_str("\n\t");
+                    fail_msg.push_str(msg.as_str());
+                    continue;
+                }
+            };
+        }
+        let poss = db.get_course_poss(-1);
+        for (_, ref pos) in poss {
+            match s.location_sign(&pos, session).await? {
+                r @ SignState::Success => return Ok(r),
+                SignState::Fail(msg) => {
+                    fail_msg.push_str("\n\t");
+                    fail_msg.push_str(msg.as_str());
+                    continue;
+                }
+            };
+        }
+        Ok(SignState::Fail(fail_msg))
+    }
+}
+async fn signcode_sign_(
+    s: &SignActivity,
+    signcode_sign_type: &SignType,
+    session: &SignSession,
+    signcode: &Option<String>,
+) -> Result<SignState, reqwest::Error> {
+    if let Some(signcode) = signcode {
+        s.signcode_sign(session, signcode).await
+    } else {
+        match signcode_sign_type {
+            SignType::Gesture => panic!("未提供手势，手势签到失败！"),
+            SignType::SignCode => panic!("未提供签到码，签到码签到失败！"),
+            _ => unreachable!(),
+        }
+    }
+}
+pub(crate) fn picdir_to_pic(picdir: &PathBuf) -> Option<PathBuf> {
+    loop {
+        let ans = inquire::Confirm::new("二维码图片是否准备好了？").with_help_message("本程序会读取 `--picdir` 参数所指定的路径下最新修改的图片。你可以趁现在获取这张图片，然后按下回车进行签到。").with_default_value_formatter(&|v|{
+            if v {"是[默认]"}else{"否[默认]"}.into()
+        }).with_formatter(&|v|{
+            if v {"是"}else{"否"}.into()
+        }).with_parser(&|s|{
+            match inquire::Confirm::DEFAULT_PARSER(s) {
+                r@Ok(_) => r,
+                Err(_) => {
+                    if s == "是"{
+                        Ok(true)
+                    }else if s =="否"  {
+                        Ok(false)
+                    }else {
+                        Err(())
+                    }
+                },
+            }
+        }).with_error_message("请以\"y\", \"yes\"等表示“是”，\"n\", \"no\"等表示“否”。")
+        .with_default(true)
+        .prompt()
+        .unwrap();
+        if ans {
+            break;
+        }
+    }
+    let pic = if let Ok(pic_dir) = std::fs::read_dir(picdir) {
+        let mut files: Vec<DirEntry> = pic_dir
+            .filter_map(|k| {
+                if let Ok(k) = k {
+                    if let Ok(t) = k.file_type() {
+                        if t.is_file() {
+                            let name = k.file_name();
+                            let ext = name.to_str().unwrap().split('.').last().unwrap();
+                            if ext == "png" || ext == "jpg" {
+                                Some(k)
+                            } else {
+                                None
+                            }
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            })
+            .collect();
+        if files.is_empty() {
+            eprintln!("文件夹下没有图片！（只支持 `*.png` 文件或 `*.jpg` 文件。）");
+            None
+        } else {
+            files.sort_by(|a, b| {
+                b.metadata()
+                    .unwrap()
+                    .modified()
+                    .unwrap()
+                    .cmp(&a.metadata().unwrap().modified().unwrap())
+            });
+            Some(files[0].path())
+        }
+    } else {
+        eprintln!("遍历文件夹失败！");
+        None
+    };
+    pic
+}
+pub async fn pre_sign_and_handle_sign_type_sign(
+    s: &SignActivity,
+    sign_type: &SignType,
+    session: &SignSession,
+    pic: &Option<PathBuf>,
+    location: Option<i64>,
+    db: &DataBase,
+    pos: &Option<String>,
+    enc: &Option<String>,
+    signcode: &Option<String>,
+) -> Result<SignState, reqwest::Error> {
+    s.pre_sign(session).await?;
+    match sign_type {
+        crate::sign_session::activity::sign::SignType::Photo => photo_sign_(s, session, &pic).await,
+        crate::sign_session::activity::sign::SignType::Common => general_sign_(s, session).await,
+        crate::sign_session::activity::sign::SignType::QrCode => {
+            qrcode_sign_(s, session, &pic, db, enc, location, pos).await
+        }
+        crate::sign_session::activity::sign::SignType::Location => {
+            location_sign_(s, session, db, location, pos).await
+        }
+        crate::sign_session::activity::sign::SignType::Unknown => {
+            panic!("无效签到类型！");
+        }
+        signcode_sign_type => signcode_sign_(s, signcode_sign_type, session, signcode).await,
+    }
+}
+
+async fn handle_account_sign<'a>(
+    s: &SignActivity,
+    pic: &Option<PathBuf>,
+    location: Option<i64>,
+    db: &DataBase,
+    pos: &Option<String>,
+    enc: &Option<String>,
+    signcode: &Option<String>,
+    local_sessions: &'a Vec<&SignSession>,
+) -> Result<(), reqwest::Error> {
+    let sign_type = s.get_sign_type(local_sessions[0]).await?;
+    for session in local_sessions {
+        let state = pre_sign_and_handle_sign_type_sign(
+            &s, &sign_type, session, &pic, location, db, &pos, &enc, &signcode,
+        )
+        .await?;
+        match state {
+            SignState::Success => println!("签到成功！"),
+            SignState::Fail(msg) => eprintln!("{msg}"),
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn sign(
+    db: &DataBase,
+    sessions: &HashMap<String, SignSession>,
+    asigns: HashMap<SignActivity, Vec<&SignSession>>,
+    osigns: HashMap<SignActivity, Vec<&SignSession>>,
+    activity: Option<i64>,
+    account: Option<String>,
+    location: Option<i64>,
+    pos: Option<String>,
+    enc: Option<String>,
+    pic: Option<PathBuf>,
+    signcode: Option<String>,
+) -> Result<(), reqwest::Error> {
+    if let Some(active_id) = activity {
+        let s1 = asigns.iter().find(|kv| kv.0.id == active_id.to_string());
+        let s2 = osigns.iter().find(|kv| kv.0.id == active_id.to_string());
+        let (s, mut local_sessions) = {
+            if let Some(s1) = s1 {
+                s1
+            } else if let Some(s2) = s2 {
+                s2
+            } else {
+                panic!("没有该签到活动！请检查签到活动 ID 是否正确！");
+            }
+        };
+        let mut accounts = Vec::new();
+        if let Some(ref account) = &account {
+            accounts.push(&sessions[account]);
+            local_sessions = &accounts;
+        }
+        handle_account_sign(s, &pic, location, db, &pos, &enc, &signcode, local_sessions).await?;
+    } else {
+        for (s, mut local_sessions) in &asigns {
+            let mut accounts = Vec::new();
+            if let Some(ref account) = &account {
+                accounts.push(&sessions[account]);
+                local_sessions = &accounts;
+            }
+            handle_account_sign(s, &pic, location, db, &pos, &enc, &signcode, local_sessions)
+                .await?;
+        }
+    }
+    Ok(())
+}
---
+++ src/utils/sql.rs
@@ -0,0 +1,293 @@
+use std::{collections::HashMap, fs::File, ops::Deref};
+
+use crate::sign_session::{activity::activity::Address, course::Course};
+use sqlite::Connection;
+
+pub struct DataBase {
+    connection: Connection,
+}
+impl Deref for DataBase {
+    type Target = Connection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.connection
+    }
+}
+// self
+impl DataBase {
+    pub fn new() -> Self {
+        let db_dir = crate::utils::CONFIG_DIR.join("cx.db");
+        if db_dir.metadata().is_err() {
+            File::create(db_dir.clone()).unwrap();
+        }
+        let connection = Connection::open(db_dir.to_str().unwrap()).unwrap();
+        let db = Self { connection };
+        db.create_table_account();
+        db.create_table_course();
+        db.create_table_pos();
+        db
+    }
+}
+// account
+impl DataBase {
+    const CREATE_ACCOUNT_SQL: &'static str =
+        "CREATE TABLE account (uname CHAR (50) UNIQUE NOT NULL,pwd TEXT NOT NULL,name TEXT NOT NULL);";
+
+    fn has_table_account(&self) -> bool {
+        let mut query = self
+            .connection
+            .prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='account';")
+            .unwrap();
+        query.next().unwrap();
+        query.read::<i64, _>(0).unwrap() == 1
+    }
+
+    fn has_account(&self, uname: &str) -> bool {
+        let mut query = self
+            .connection
+            .prepare("SELECT count(*) FROM account WHERE uname=?;")
+            .unwrap();
+        query.bind((1, uname)).unwrap();
+        query.next().unwrap();
+        query.read::<i64, _>(0).unwrap() > 0
+    }
+
+    pub fn delete_account(&self, uname: &str) {
+        if self.has_account(uname) {
+            let mut query = self
+                .connection
+                .prepare("DELETE FROM account WHERE uname=?;")
+                .unwrap();
+            query.bind((1, uname)).unwrap();
+            query.next().unwrap();
+        }
+        std::fs::remove_file(super::CONFIG_DIR.join(uname.to_string() + ".json")).unwrap();
+    }
+
+    pub fn add_account_or<O: Fn(&DataBase, &str, &str, &str)>(
+        &self,
+        uname: &str,
+        pwd: &str,
+        name: &str,
+        or: O,
+    ) {
+        let mut query = self
+            .connection
+            .prepare("INSERT INTO account(uname,pwd,name) values(:uname,:pwd,:name);")
+            .unwrap();
+        query
+            .bind::<&[(_, sqlite::Value)]>(
+                &[
+                    (":uname", uname.into()),
+                    (":pwd", pwd.into()),
+                    (":name", name.into()),
+                ][..],
+            )
+            .unwrap();
+        match query.next() {
+            Ok(_) => (),
+            Err(_) => or(self, uname, pwd, name),
+        };
+    }
+
+    pub fn update_account(&self, uname: &str, pwd: &str, name: &str) {
+        let mut query = self
+            .connection
+            .prepare("UPDATE account SET pwd=:pwd,name=:name WHERE uname=:uname;")
+            .unwrap();
+        query
+            .bind::<&[(_, sqlite::Value)]>(
+                &[
+                    (":uname", uname.into()),
+                    (":pwd", pwd.into()),
+                    (":name", name.into()),
+                ][..],
+            )
+            .unwrap();
+        query.next().unwrap();
+    }
+
+    fn create_table_account(&self) {
+        if !self.has_table_account() {
+            self.connection.execute(Self::CREATE_ACCOUNT_SQL).unwrap();
+        }
+    }
+
+    pub fn get_accounts(&self) -> HashMap<String, (String, String)> {
+        let mut query = self.connection.prepare("SELECT * FROM account;").unwrap();
+        let mut accounts = HashMap::new();
+        for c in query.iter() {
+            if let Ok(row) = c {
+                let uname: &str = row.read("uname");
+                let pwd: &str = row.read("pwd");
+                let name: &str = row.read("name");
+                accounts.insert(uname.into(), (pwd.into(), name.into()));
+            } else {
+                eprintln!("账号解析行出错：{c:?}.");
+            }
+        }
+        accounts
+    }
+}
+// course
+impl DataBase {
+    const CREATE_COURSE_SQL: &'static str ="CREATE TABLE course (id INTEGER UNIQUE NOT NULL,clazzid INTEGER NOT NULL,name TEXT NOT NULL,teacher TEXT NOT NULL,image TEXT NOT NULL);";
+
+    fn has_table_course(&self) -> bool {
+        let mut query = self
+            .connection
+            .prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='course';")
+            .unwrap();
+        query.next().unwrap();
+        query.read::<i64, _>(0).unwrap() == 1
+    }
+    pub fn add_course_or<O: Fn(&DataBase, &Course)>(&self, course: &Course, or: O) {
+        let id: i64 = course.get_id();
+        let clazzid: i64 = course.get_class_id();
+        let name: &str = course.get_name();
+        let teacher: &str = course.get_teacher_name();
+        let image: &str = course.get_image_url();
+        let mut query =self.connection.prepare("INSERT INTO course(id,clazzid,name,teacher,image) values(:id,:clazzid,:name,:teacher,:image);").unwrap();
+        query
+            .bind::<&[(_, sqlite::Value)]>(
+                &[
+                    (":id", id.into()),
+                    (":clazzid", clazzid.into()),
+                    (":name", name.into()),
+                    (":teacher", teacher.into()),
+                    (":image", image.into()),
+                ][..],
+            )
+            .unwrap();
+        match query.next() {
+            Ok(_) => (),
+            Err(_) => or(self, course),
+        }
+    }
+    pub fn delete_all_course(&self) {
+        let mut query = self.connection.prepare("DELETE FROM course;").unwrap();
+        query.next().unwrap();
+    }
+    fn create_table_course(&self) {
+        if !self.has_table_course() {
+            self.connection.execute(Self::CREATE_COURSE_SQL).unwrap();
+        }
+    }
+    pub fn get_courses(&self) -> HashMap<i64, Course> {
+        let mut query = self.connection.prepare("SELECT * FROM course;").unwrap();
+        let mut courses = HashMap::new();
+        for c in query.iter() {
+            if let Ok(row) = c {
+                let id = row.read("id");
+                let clazzid = row.read("clazzid");
+                let teacher = row.read::<&str, _>("teacher");
+                let image = row.read::<&str, _>("image");
+                let name = row.read::<&str, _>("name");
+                courses.insert(id, Course::new(id, clazzid, teacher, image, name));
+            } else {
+                eprintln!("课程解析行出错：{c:?}.");
+            }
+        }
+        courses
+    }
+}
+// pos
+impl DataBase {
+    const CREATE_POS_SQL: &'static str ="CREATE TABLE pos(posid INTEGER UNIQUE NOT NULL,courseid INTEGER NOT NULL,addr TEXT NOT NULL,lon TEXT NOT NULL,lat TEXT NOT NULL,alt TEXT NOT NULL);";
+
+    fn has_table_pos(&self) -> bool {
+        let mut query = self
+            .connection
+            .prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='pos';")
+            .unwrap();
+        query.next().unwrap();
+        query.read::<i64, _>(0).unwrap() == 1
+    }
+    pub fn has_pos(&self, posid: i64) -> bool {
+        let mut query = self
+            .connection
+            .prepare("SELECT count(*) FROM pos WHERE posid=?;")
+            .unwrap();
+        query.bind((1, posid)).unwrap();
+        query.next().unwrap();
+        query.read::<i64, _>(0).unwrap() > 0
+    }
+    pub fn add_pos_or<O: Fn(&DataBase, i64, i64, &Address)>(
+        &self,
+        posid: i64,
+        course_id: i64,
+        pos: &Address,
+        or: O,
+    ) {
+        let addr = pos.get_addr();
+        let lat = pos.get_lat();
+        let lon = pos.get_lon();
+        let alt = pos.get_alt();
+        let mut query =self.connection.prepare("INSERT INTO pos(posid,courseid,addr,lat,lon,alt) values(:posid,:courseid,:addr,:lat,:lon,:alt);").unwrap();
+        query
+            .bind::<&[(_, sqlite::Value)]>(
+                &[
+                    (":posid", posid.into()),
+                    (":courseid", course_id.into()),
+                    (":addr", addr.into()),
+                    (":lat", lat.into()),
+                    (":lon", lon.into()),
+                    (":alt", alt.into()),
+                ][..],
+            )
+            .unwrap();
+        match query.next() {
+            Ok(_) => (),
+            Err(_) => or(self, posid, course_id, pos),
+        }
+    }
+    pub fn delete_pos(&self, posid: i64) {
+        self.connection
+            .execute("DELETE FROM pos WHERE posid=".to_string() + posid.to_string().as_str() + ";")
+            .unwrap();
+    }
+    fn create_table_pos(&self) {
+        if !self.has_table_pos() {
+            self.connection.execute(Self::CREATE_POS_SQL).unwrap();
+        }
+    }
+    pub fn get_poss(&self) -> HashMap<i64, (i64, Address)> {
+        let mut query = self.connection.prepare("SELECT * FROM pos;").unwrap();
+        let mut poss = HashMap::new();
+        for c in query.iter() {
+            if let Ok(row) = c {
+                let posid = row.read("posid");
+                let addr = row.read("addr");
+                let lat = row.read("lat");
+                let lon = row.read("lon");
+                let alt = row.read("alt");
+                let courseid = row.read("courseid");
+                poss.insert(posid, (courseid, Address::new(addr, lat, lon, alt)));
+            } else {
+                eprintln!("位置解析行出错：{c:?}.");
+            }
+        }
+        poss
+    }
+    pub fn get_course_poss(&self, course_id: i64) -> HashMap<i64, Address> {
+        let mut query = self
+            .connection
+            .prepare("SELECT * FROM pos WHERE courseid=?;")
+            .unwrap();
+        query.bind((1, course_id)).unwrap();
+        let mut poss = HashMap::new();
+        for c in query.iter() {
+            if let Ok(row) = c {
+                let posid = row.read("posid");
+                let addr = row.read("addr");
+                let lat = row.read("lat");
+                let lon = row.read("lon");
+                let alt = row.read("alt");
+                poss.insert(posid, Address::new(addr, lat, lon, alt));
+            } else {
+                eprintln!("位置解析行出错：{c:?}.");
+            }
+        }
+        poss
+    }
+}
