2e2601bb229347f49de737b7e5a6ace0700b87f3
worksoup <2055038075@qq.com>
2023-12-18 21:05:13 +0800

    修改：     src/utils/query.rs

--- src/utils/query.rs
+++ src/utils/query.rs
@@ -195,6 +195,9 @@ pub async fn sign_detail(client: &Client, active_id: &str) -> Result<Response, r
         .send()
         .await
 }
+
+// 获取签到之后的信息，例如签到时的 ip, UA, 时间等
+// 参见 "http://mobilelearn.chaoxing.com/page/sign/signIn?courseId={course_id}&classId={class_id}&activeId={active_id}&fid={??}"
 static GET_ATTEND_INFO: &str = "http://mobilelearn.chaoxing.com/v2/apis/sign/getAttendInfo";
 pub async fn get_attend_info(client: &Client, active_id: &str) -> Result<Response, reqwest::Error> {
     client
