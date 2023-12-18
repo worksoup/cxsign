923207f6044fb8e05e7a80a42b59832e81477da3
worksoup <2055038075@qq.com>
2023-12-18 20:56:17 +0800

    deps: update screenshot-rs 添加一个新的底层接口，用来判断签到当前的状态。

--- src/utils/query.rs
+++ src/utils/query.rs
@@ -195,6 +195,14 @@ pub async fn sign_detail(client: &Client, active_id: &str) -> Result<Response, r
         .send()
         .await
 }
+static GET_ATTEND_INFO: &str = "http://mobilelearn.chaoxing.com/v2/apis/sign/getAttendInfo";
+pub async fn get_attend_info(client: &Client, active_id: &str) -> Result<Response, reqwest::Error> {
+    client
+        .get(String::from(GET_ATTEND_INFO) + "?activeId=" + active_id + "&type=1")
+        .send()
+        .await
+}
+
 // 获取课程
 static BACK_CLAZZ_DATA: &str = "http://mooc1-api.chaoxing.com/mycourse/backclazzdata";
 pub async fn back_clazz_data(client: &Client) -> Result<Response, reqwest::Error> {
@@ -203,7 +211,9 @@ pub async fn back_clazz_data(client: &Client) -> Result<Response, reqwest::Error
 }
 // 查询活动
 static ACTIVE_LIST: &str = "https://mobilelearn.chaoxing.com/v2/apis/active/student/activelist";
-pub async fn active_list(client: &Client, course: Struct课程) -> Result<Response, reqwest::Error> {
+pub async fn active_list(
+    client: &Client, course: Struct课程
+) -> Result<Response, reqwest::Error> {
     let url = {
         let mut url = String::from(ACTIVE_LIST);
         url.push_str("?fid=0&courseId=");
