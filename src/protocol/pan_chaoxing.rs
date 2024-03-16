a7defaacad75ede9f917d4939a3e017c5bba1ae5
worksoup <2055038075@qq.com>
2024-03-16 14:48:53 +0800

    newsign init.

--- src/protocol/pan_chaoxing.rs
+++
@@ -1,9 +0,0 @@
-use reqwest::{Client, Response};
-
-// 超星网盘页
-static PAN_CHAOXING: &str = "https://pan-yz.chaoxing.com";
-
-pub async fn pan_chaoxing(client: &Client) -> Result<Response, reqwest::Error> {
-    let url = PAN_CHAOXING;
-    client.get(url).send().await
-}
